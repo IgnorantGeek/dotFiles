450e168e25653082a30ce62e5c0296864e86ea06
Nick Heisler <nheisler@iastate.edu>
2020-01-22 13:52:55 -0600

    updated move.sh

--- move.sh
+++ move.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-if [ "$1" = "Movies" ] || [ "$1" = "TVshows" ]; then
-  for var in "$@"
-    do
-      if [ "$var" != "$1" ]; then
-        echo "Transfering $var to Airforce1..."
-        scp -r "$var" Plex:/media/storageHDD/plexmedia/$1
-        echo "Finished."
-      fi
-    done
+if [ "$1" = "-m" ] || [ "$1" = "-M" ]; then
+for var in "$@"
+  do
+    if [ "$var" != "$1" ]; then
+      echo "Transfering $var to Airforce1..."
+      scp -r "$var" Plex:/media/storageHDD/plexmedia/Movies/
+      echo "Finished."
+    fi
+  done
+elif [ "$1" = "-t"] || [ "$1" = "-T" ]; then
+for var in "$@"
+  do
+    if [ "$var" != "$1" ]; then
+      echo "Transfering $var to Airforce1..."
+      scp -r "$var" Plex:/media/storageHDD/plexmedia/TVshows/
+      echo "Finished."
+    fi
+  done
 else
-  echo "Incorrect destination name."
+  echo "Destination not specified. Enter '-m' for movies and '-t' for tv shows."
 fi
