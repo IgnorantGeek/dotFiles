2f350eea7a7ef60fde0ab55ec8de7c025362e66a
Nick Heisler <nickheisler63@gmail.com>
2020-06-09 21:50:59 -0500

    working on zsh config install script

---
+++ zsh-config-install.sh
@@ -0,0 +1,17 @@
+# install zsh (make sure zsh is installed)
+sudo apt install zsh
+
+# install oh-my-zsh framework
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# need to say yes to changing default shell to zsh
+
+# install powerlevel9k
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+# install powerline fonts
+sudo apt install fonts-powerline
+
+
+# install zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
